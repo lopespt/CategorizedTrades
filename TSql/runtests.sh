47cbd7f28c9218e9f608aef2ab7bba68c74f7020
Guilherme Wachs Lopes <guilhermewachs@gmail.com>
2022-07-10 19:39:27 -0300

    Removed commented lines from runtests.sh

--- TSql/runtests.sh
+++ TSql/runtests.sh
@@ -6,11 +6,3 @@ docker run --name $containerName --rm -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=root@12
 sleep 2
 docker exec  $containerName bash /tmp/TSql/TestFiles/test_script.sh  
 docker stop $containerName  > /dev/null
-
-#exec dbo.ClassifyTrades;
-#
-#select *
-#from Trades
-#         left join dbo.ClientSectors CS on CS.ClientSectorId = Trades.ClientSectorId
-#         left join dbo.TradeRisks TR on Trades.TradeRiskId = TR.TradeRiskId;
-    
